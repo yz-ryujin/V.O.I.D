0cd0aac10e96d7d35a75431175971a329a124ee6
Franklin William <ui.develloper@gmail.com>
2025-09-14 22:44:00 -0300

    feat(Core): Adiciona a classe GameManager e estrutra a pasta Core
    Cria a pasta Core para abrigar as classes centrais do gerenciamento do jogo, conforme a arquitetura planejada.
    
    Adiciona a classe GameManager, que será responsável pelo loop principal da campanha e pelos estados do jogo. Atualiza o Program.cs para delegar o início da execução ao GameManager, limpando o ponto de entrada da aplicação.

---
+++ Core/GameManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading;
+
+namespace Void.Core
+{
+    public class GameManager {
+        public void StartCampaing()
+        {
+            Console.WriteLine("O prólogo começará aqui...");
+
+        }
+
+    }
+}
