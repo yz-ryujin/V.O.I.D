0ffa0acf622af995de4f3ac34d200ffa77450f26
Franklin William <ui.develloper@gmail.com>
2025-09-15 00:02:18 -0300

    feat(Player): Adiciona uma lista de habilidades ao jogador
    Modifica a classe Player para suportar um sistema de habilidades, adicionando uma propriedade 'List<Skill>'.
    
    - A lista de habilidade é inicializada no construtor do Player.
    - A classe PrologueCutscene foi atualizada para, no momento da criação da Alara, adicionar a instância da habilidade 'FlechaPerfurante' à sua lista de skills, efetivamente 'ensinando' a habilidade à personagem.

--- Entities/Characters/Player.cs
+++ Entities/Characters/Player.cs
@@ -1,8 +1,17 @@
+using System;
+using Void.Systems.Combat;
+using System.Collections.Generic;
+
+
 namespace Void.Entities.Characters
 {
     // Classe Player que herda de Entity
     public class Player : Entity
     {
+
+        // Nova Propriedade: Uma lista para guardar todas as habilidades do personagem.
+        public List<Skill> Skills { get; private set; }
+
         // Construtor da classe Player
         public Player(string name, int health, int damage, int range)
         {
@@ -12,6 +21,8 @@ namespace Void.Entities.Characters
             AttackDamage = damage;
             AttackRange = range;
             Position = 2; // Posição inicial do jogador
+
+            Skills = new List<Skill>();
         }
     }
 }
